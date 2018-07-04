e04581de17b741b7a3e6c40280bf1fa2ce3e3f92
Spor <sporeyo89@gmail.com>
2018-07-04 12:53:13 +0300

    changed to template strings

--- script/test.js
+++ script/test.js
@@ -1,7 +1,6 @@
-window.onload = function what(){ // Событие onload на window срабатывает, когда загружается вся страница, включая ресурсы на ней – стили, картинки, ифреймы и т.п.
-    
-    var cardsData = [
-        {
+window.onload = function what() { // Событие onload на window срабатывает, когда загружается вся страница, включая ресурсы на ней – стили, картинки, ифреймы и т.п.
+
+    var cardsData = [{
             "productId": "8b051a82-e051-11e4-afe5-00259036a192",
             "code": "00000142709",
             "title": "Плитка облицовочная Сан-Ремо 7 200х200х7 мм белая (26 шт=1.04 кв.м)",
@@ -2303,48 +2302,114 @@ window.onload = function what(){ // Событие onload на window сраба
         }
     ]
 
-    var output="<div class='products_page pg_0'>";
+    var output = "<div class='products_page pg_0'>";
 
     for (var i in cardsData) {
 
-        output+=
-        "<div class='product product_horizontal'>" +
-        "<span class='product_code'>"+"Код:"+ + cardsData[i].code +"</span>" +
-        "<div class='product_status_tooltip_container'>" + "<span class='product_status'>" + "Наличие" +"</span>" + "</div>" + 
-        "<div class='product_photo'>"+"<a href='#' class='url--link product__link'>"+"<img src="+cardsData[i].primaryImageUrl+">"+"</a>"+"</div>"+
-        "<div class='product_description'>" + "<a href='#' class='product__link'>" + cardsData[i].title +"</a>" +"</div>"+
-        "<div class='product_tags hidden-sm'>" + "<p>" + "Могут понадобиться:" + "</p>" + "<a href='#' class='url--link'>" + cardsData[i].assocProducts +"</a>"+ "</div>"+
-        "<div class='product_units'>"+"<div class='unit--wrapper'>"+"<div class='unit--select unit--active'>"+"<p class='ng-binding'>" + "За м. кв." + "</p>"+"</div>"+"<div class='unit--select'>"+"<p class='ng-binding'>" + "За упаковку" + "</p>"+"</div>"+"</div>"+"</div>"+
-        "<p class='product_price_club_card'>" + "<span class='product_price_club_card_text'>" + "По карте<br>клуба" + "</span>"+"<span class='goldPrice'>" + cardsData[i].priceGoldAlt + "</span>"+"<span class='rouble__i black__i'>"+"<svg version='1.0' id='rouble__b' xmlns='http://www.w3.org/2000/svg' x='0' y='0' width='30px' height='22px' viewBox='0 0 50 50' enable-background='new 0 0 50 50' xml:space='preserve'>"+"<use xmlns:xlink='http://www.w3.org/1999/xlink' xlink:href='#rouble_black'>"+"</use>"+"</svg>"+"</span>"+"</p>"+
-        "<p class='product_price_default'>" + "<span class='retailPrice'>" + cardsData[i].priceRetailAlt + "</span>"+"<span class='rouble__i black__i'>"+"<svg version='1.0' id='rouble__b' xmlns='http://www.w3.org/2000/svg' x='0' y='0' width='30px' height='22px' viewBox='0 0 50 50' enable-background='new 0 0 50 50' xml:space='preserve'>"+"<use xmlns:xlink='http://www.w3.org/1999/xlink' xlink:href='#rouble_gray'>"+"</use>"+"</svg>"+"</span>"+"</p>"+
-        "<div class='product_price_points'>"+"<p class='ng-binding'>"+"Можно купить за 231,75 балла"+"</p>"+"</div>"+
-        "<div class='list--unit-padd'>"+"</div>"+
-        "<div class='list--unit-desc'>"+"<div class='unit--info'>"+"<div class='unit--desc-i'>"+"</div>"+"<div class='unit--desc-t'>"+"<p>"+"<span class='ng-binding'>"+"Продается упаковками:"+"</span>"+"<span class='unit--infoInn'>"+"1 упак. = 2.47 м. кв. "+"</span>"+"</p>"+"</div>"+"</div>"+"</div>"+
-        "<div class='product__wrapper'>"+"<div class='product_count_wrapper'>"+"<div class='stepper'>"+"<input class='product__count stepper-input' type='text' value='1'>"+"<span class='stepper-arrow up'>"+"</span>"+"<span class='stepper-arrow down'>"+"</span>"+"</div>"+"</div>"+"<span class='btn btn_cart' data-url='/cart/' data-product-id="+cardsData[i].productId+">"+"<svg class='ic ic_cart'>"+"<use xmlns:xlink='http://www.w3.org/1999/xlink' xlink:href='#cart'>"+"</use>"+"</svg>"+"<span class='ng-binding'>"+"В корзину"+"</span>"+"</span>"+"</div>"
+        output +=
+            `<div class='product product_horizontal'>
+                <span class='product_code'>Код:${cardsData[i].code}</span> 
+                <div class='product_status_tooltip_container'> 
+                    <span class='product_status'>Наличие</span>
+                </div>  
+                <div class='product_photo'>
+                    <a href='#' class='url--link product__link'><img src=${cardsData[i].primaryImageUrl}></a>
+                </div>
+                <div class='product_description'>
+                    <a href='#' class='product__link'>${cardsData[i].title}</a>
+                </div>
+                <div class='product_tags hidden-sm'>
+                    <p>Могут понадобиться:</p>
+                    <a href='#' class='url--link'>${cardsData[i].assocProducts}</a>
+                </div>
+                <div class='product_units'>
+                    <div class='unit--wrapper'>
+                        <div class='unit--select unit--active'>
+                            <p class='ng-binding'>За м. кв.</p>
+                        </div>
+                        <div class='unit--select'>
+                            <p class='ng-binding'>За упаковку</p>
+                        </div>
+                    </div>
+                </div>
+                <p class='product_price_club_card'>
+                    <span class='product_price_club_card_text'>По карте<br>клуба</span>
+                    <span class='goldPrice'>${cardsData[i].priceGoldAlt}</span>
+                    <span class='rouble__i black__i'>
+                        <svg version='1.0' id='rouble__b' xmlns='http://www.w3.org/2000/svg' x='0' y='0' width='30px' height='22px' viewBox='0 0 50 50' enable-background='new 0 0 50 50' xml:space='preserve'>
+                            <use xmlns:xlink='http://www.w3.org/1999/xlink' xlink:href='#rouble_black'>
+                            </use>
+                        </svg>
+                    </span>
+                </p>
+                <p class='product_price_default'>
+                    <span class='retailPrice'>${cardsData[i].priceRetailAlt}</span>
+                    <span class='rouble__i black__i'>
+                        <svg version='1.0' id='rouble__b' xmlns='http://www.w3.org/2000/svg' x='0' y='0' width='30px' height='22px' viewBox='0 0 50 50' enable-background='new 0 0 50 50' xml:space='preserve'>
+                            <use xmlns:xlink='http://www.w3.org/1999/xlink' xlink:href='#rouble_gray'>
+                            </use>
+                        </svg>
+                    </span>
+                </p>
+                <div class='product_price_points'>
+                    <p class='ng-binding'>Можно купить за 231,75 балла</p>
+                </div>
+                <div class='list--unit-padd'>
+                </div>
+                <div class='list--unit-desc'>
+                    <div class='unit--info'>
+                        <div class='unit--desc-i'>
+                    </div>
+                    <div class='unit--desc-t'>
+                        <p>
+                            <span class='ng-binding'>Продается упаковками:</span>
+                            <span class='unit--infoInn'>1 упак. = 2.47 м. кв.</span>
+                        </p>
+                    </div>
+                </div>
+            </div>
+            <div class='product__wrapper'>
+            <div class='product_count_wrapper'>
+                <div class='stepper'>
+                    <input class='product__count stepper-input' type='text' value='1'>
+                        <span class='stepper-arrow up'>
+                        </span>
+                        <span class='stepper-arrow down'>
+                        </span>
+                </div>
+            </div>
+            <span class='btn btn_cart' data-url='/cart/' data-product-id=${cardsData[i].productId}>
+                <svg class='ic ic_cart'>
+                    <use xmlns:xlink='http://www.w3.org/1999/xlink' xlink:href='#cart'>
+                    </use>
+                </svg>
+            <span class='ng-binding'>В корзину</span>
+            </span>
+        </div>`
 
-       output+="</div>";
-       
+        output += "</div>";
+
     }
 
-    output+="</div>";
+    output += "</div>";
 
-    document.getElementById("products_section").innerHTML=output;
+    document.getElementById("products_section").innerHTML = output;
 
-    $(document).ready(function() {
-            $('.up').click(function () {
-                var $input = $(this).parent().find('input');
-                $input.val(parseInt($input.val()) + 1);
-                $input.change();
-                return false;
-            });
-            $('.down').click(function () {
-                var $input = $(this).parent().find('input');
-                var count = parseInt($input.val()) - 1;
-                count = count < 1 ? 1 : count;
-                $input.val(count);
-                $input.change();
-                return false;
-            });
+    $(document).ready(function () {
+        $('.up').click(function () {
+            var $input = $(this).parent().find('input');
+            $input.val(parseInt($input.val()) + 1);
+            $input.change();
+            return false;
         });
+        $('.down').click(function () {
+            var $input = $(this).parent().find('input');
+            var count = parseInt($input.val()) - 1;
+            count = count < 1 ? 1 : count;
+            $input.val(count);
+            $input.change();
+            return false;
+        });
+    });
 
 };
